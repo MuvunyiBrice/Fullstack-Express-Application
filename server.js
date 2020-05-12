c6f51a8797a5c5740e11d430b98fa1f72fbb33f8
Brice M <movebrice@gmail.com>
2020-05-12 14:14:20 +0200

    added search feature

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -24,6 +27,7 @@ db.on("error", (error) => console.error(error));
 db.on("open", () => console.log("Connected to DB"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server up on port 3000");
