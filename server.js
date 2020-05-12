ef8d28ed40d62e6ef4822b895a3802215a9fe556
Brice M <movebrice@gmail.com>
2020-05-12 11:50:27 +0200

    added DB connection

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
@@ -9,6 +13,15 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.on("open", () => console.log("Connected to DB"));
 
 app.use("/", indexRouter);
 
