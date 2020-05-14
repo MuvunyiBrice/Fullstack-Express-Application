7a6bb9048931626d37d226d44b4f5054548484ec
Brice M <movebrice@gmail.com>
2020-05-14 19:24:00 +0200

    finalised author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/Author");
+const Book = require("../models/Book");
 
 // Get all authors
 router.get("/", async (req, res) => {
@@ -11,7 +12,7 @@ router.get("/", async (req, res) => {
   try {
     const authors = await Author.find(searchOptions);
     res.render("authors/index", { authors: authors, searchOptions: req.query });
-  } catch (error) {
+  } catch {
     res.redirect("/");
   }
 });
@@ -28,13 +29,69 @@ router.post("/", async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
-    //   res.redirect(`authors/${newAuthor.id}`);
-    res.redirect(`authors`);
+    res.redirect(`authors/${newAuthor.id}`);
   } catch (error) {
     res.render("authors/new", {
       author: author,
       errorMessage: "Error creating new Author",
     });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/show", {
+      author,
+      booksByAuthor: books,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", { author });
+  } catch (error) {
+    res.redirect("/authors");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch (error) {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("authors/edit", {
+        author: author,
+        errorMessage: "Error updating  Author",
+      });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect(`/authors`);
+  } catch (error) {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/authors/${author.id}`);
+    }
   }
 });
 
